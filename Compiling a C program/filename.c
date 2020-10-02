5acc00fa6ee47d29b34e9e6399d107e8ade7b1d8
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-10-02 09:48:20 -0400

    Initial commit

---
+++ Compiling a C program/filename.c
@@ -0,0 +1,10 @@
+// program for the addtion of two numbers
+#include<stdio.h>
+#define add(a,b) (a+b) // using macros
+int main() 
+{
+    int a = 5, b = 4;
+    printf("Addition is: %d\n", add(a,b));
+    return 0;
+    
+}
